df577fe40e3e12115f747947236e636c871eb633
codymcnamara <codyjamesmcnamara@gmail.com>
2023-09-03 11:31:08 -0700

    add navbar and toggle functionality

---
+++ src/Navbar.jsx
@@ -0,0 +1,59 @@
+import { useState, useRef } from 'react';
+import { FaBars } from 'react-icons/fa';
+import { links, social } from './data';
+import logo from './logo.svg';
+
+const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+  
+  const toggleLinks = () => {
+    console.log(linksRef);
+    setShowLinks(!showLinks);
+    // if(showLinks){
+    //     let height = linksRef.current.getBoundingClientRect().height;
+    //     linksContainerRef.current.style.height = ;
+    // }else{
+    //     linksContainerRef.current.style.height = 0;
+    // }
+  };
+
+  let linkStyle = {
+    height: showLinks 
+        ? `${linksRef.current.getBoundingClientRect().height}px` 
+        : '0px'
+  }
+
+  return (
+    <nav>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} className='logo' alt='logo' />
+          <button className='nav-toggle' onClick={toggleLinks}>
+            <FaBars />
+          </button>
+        </div>
+
+        <div
+            ref={linksContainerRef}
+            style={linkStyle}
+            className="links-container"
+        >
+            <ul className='links' ref={linksRef}>
+              {links.map((link) => {
+                const { id, url, text } = link;
+                return (
+                  <li key={id}>
+                    <a href={url}>{text}</a>
+                  </li>
+                );
+              })}
+            </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
